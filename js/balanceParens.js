2ce65ec7ff34b2077a1a1a550705a158a3124fe3
Garrett Adams <garrett.adams52@gmail.com>
2022-05-30 20:15:32 -0400

    finished js

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,29 @@
 balanceParens = (str) =>
 {
+    let stack = []
+    str = str.split('')
+    for(let i = 0; i<str.length;i++){
+       
+        if(str[i] === '(' || str[i] === ')'){
+            if(str[i] === '(')
+                stack.push(i)
+            else if(str[i] === ')' && (stack.length>0))
+                stack.pop()
+            else if(str[i] === ')' && (stack.length<=0)){
+                str.splice(i,1)
+                i -=1
+            }
+        }
+    }
+  
+    while(stack.length>0){
+        str.splice((stack[stack.length-1]),1)
+        stack.pop()
+    }
 
+    return str.join('')
 }
+
+
 
 module.exports = { balanceParens }
